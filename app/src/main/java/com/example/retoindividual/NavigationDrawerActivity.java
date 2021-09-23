833b3ff7e258abecc54ec11c31cc34d5c1761582
Christian <crojasantillan@gmail.com>
2021-09-23 10:21:33 +0200

    borrado de action bar en menu (tres puntitos que aparecen en la barra principal)

--- app/src/main/java/com/example/retoindividual/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/retoindividual/NavigationDrawerActivity.java
@@ -53,13 +53,6 @@ public class NavigationDrawerActivity extends AppCompatActivity
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
-        return true;
-    }
 
     @Override
     public boolean onSupportNavigateUp()
