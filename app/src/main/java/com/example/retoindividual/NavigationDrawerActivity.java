3f11130b9f1a2df436c5505a9567ad8b6519459e
Christian <crojasantillan@gmail.com>
2021-09-23 13:25:24 +0200

    creacion del adapter de lista y llenado de datos

--- app/src/main/java/com/example/retoindividual/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/retoindividual/NavigationDrawerActivity.java
@@ -3,7 +3,10 @@ package com.example.retoindividual;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.GridView;
+import android.widget.ListView;
 
+import com.example.retoindividual.adaptadores.CursosAdaptador;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -16,12 +19,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.retoindividual.databinding.ActivityNavigationDrawerBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NavigationDrawerActivity extends AppCompatActivity
 {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityNavigationDrawerBinding binding;
-
+    private List<String> arrayCursos = new ArrayList<String>();
+    private ListView lvCursos = null;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -31,15 +38,7 @@ public class NavigationDrawerActivity extends AppCompatActivity
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarNavigationDrawer.toolbar);
-        binding.appBarNavigationDrawer.fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
@@ -51,13 +50,32 @@ public class NavigationDrawerActivity extends AppCompatActivity
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        llenarCursos();
+
+        lvCursos = findViewById(R.id.lvCursos);
+        CursosAdaptador adapter = new CursosAdaptador(this, 0, arrayCursos);
+        lvCursos.setAdapter(adapter);
+
     }
 
+    private void llenarCursos()
+    {
+        arrayCursos.add("Cursos:");
+        arrayCursos.add("DAM1");
+        arrayCursos.add("DAM2");
+        arrayCursos.add("ASIR1");
+        arrayCursos.add("ASIR2");
+        arrayCursos.add("SMR1");
+        arrayCursos.add("SMR2");
+        arrayCursos.add("GA1");
+        arrayCursos.add("GA2");
+    }
 
     @Override
     public boolean onSupportNavigateUp()
     {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
+        NavController navController = Navigation.findNavController( this, R.id.nav_host_fragment_content_navigation_drawer);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
---
+++ app/src/main/java/com/example/retoindividual/adaptadores/CursosAdaptador.java
@@ -0,0 +1,54 @@
+package com.example.retoindividual.adaptadores;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.retoindividual.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CursosAdaptador extends ArrayAdapter
+{
+    private List<String> arrayList = null;
+    private Context context = null;
+    public CursosAdaptador(@NonNull Context context, int resource, @NonNull List<String> arrayList)
+    {
+        super(context, resource, arrayList);
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return arrayList.size();
+    }
+
+    @Nullable
+    @Override
+    public Object getItem(int position)
+    {
+        return arrayList.get(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        LayoutInflater inflater =LayoutInflater.from(context);
+        View vista = inflater.inflate(R.layout.lista_cursos_per, parent, false);
+        TextView tvCursos = vista.findViewById(R.id.tvCursos);
+        tvCursos.setText(arrayList.get(position));
+        return vista;
+    }
+}
